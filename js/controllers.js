41e33ce59a26d32ad119cd9f0e184e1e19af54ad
WillDetlor <detl8600@mylaurier.ca>
2015-03-28 14:29:43 -0400

    Formatting and adding new event modal

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,42 @@
-var GrapeVine = angular.module("GrapeVine", []);
+var GrapeVine = angular.module("GrapeVine", ['ui.bootstrap']);
 
-GrapeVine.controller("eventControl", function($scope, $http) {
-	$http.get('events.php').success(function(data) {
+GrapeVine.controller("eventControl", function($scope, $http, $modal) {
+	
+	$http.get('php/events.php').success(function(data) {
 		$scope.events = data.events;
 	});
+
 	$scope.join = function(ev){
 		console.log(ev);
-		$http.post('join.php',{userid:"1",eventid:ev.id}).success(function(data){
+		$http.post('php/join.php',{userid:"1",eventid:ev.id}).success(function(data){
 			console.log(data);
 		});
 	};
+
+	$scope.open = function(size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'tpl/new_event.html',
+      controller: 'modInstControl',
+      size: size,
+      resolve: {}
+    });
+	}
+});
+
+GrapeVine.controller('modInstControl', function($scope, $modalInstance) {
+
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
 
 GrapeVine.directive('eventCard', function() {
   return {
-    templateUrl: 'event_card.html'
+    templateUrl: 'tpl/event_card.html'
   };
 });
