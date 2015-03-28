de09d49acbe4eb63807c236273a4f809f4dbfed5
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-03-28 09:01:10 -0400

    goodstuff

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,8 @@
 var GrapeVine = angular.module("GrapeVine", []);
 
 GrapeVine.controller("eventControl", function($scope, $http) {
-	$http.get('jsons/events.json').success(function(data) {
+	$http.get('events.php').success(function(data) {
+		console.log(data);
 		$scope.events = data.events;
 	});
 });
