3b5506eab52b4b8c7d3fd97614295a5fe28ea99a
WillDetlor <detl8600@mylaurier.ca>
2015-03-28 12:29:02 -0400

    Modal

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,31 @@
-var GrapeVine = angular.module("GrapeVine", []);
+var GrapeVine = angular.module("GrapeVine", ['ui.bootstrap']);
 
-GrapeVine.controller("eventControl", function($scope, $http) {
+GrapeVine.controller("eventControl", function($scope, $http, $modal, $log) {
+	
 	$http.get('jsons/events.json').success(function(data) {
 		$scope.events = data.events;
 	});
+
+	$scope.open = function(size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'settings_tpl.html',
+      controller: 'modInstControl',
+      size: size,
+      resolve: {}
+      }
+
+    );
+};	
+});
+
+GrapeVine.controller('modInstControl', function($scope, $modalInstance) {
+
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
