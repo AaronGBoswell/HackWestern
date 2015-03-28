aa46dac90742d38e2be4981aa7c99c23164ee232
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-03-28 11:13:13 -0400

    Revert db938df..377e994
    This rolls back to commit db938df799b63d92e7145188ab7df7d508fdac5b.

--- js/controllers.js
+++ js/controllers.js
@@ -2,14 +2,9 @@ var GrapeVine = angular.module("GrapeVine", []);
 
 GrapeVine.controller("eventControl", function($scope, $http) {
 	$http.get('events.php').success(function(data) {
+		console.log(data);
 		$scope.events = data.events;
 	});
-	$scope.join = function(ev){
-		console.log(ev);
-		$http.get('join.php',{userid:"1",projectid:ev.id}).success(function(data){
-			console.log(data);
-		});
-	};
 });
 
 GrapeVine.directive('eventCard', function() {
